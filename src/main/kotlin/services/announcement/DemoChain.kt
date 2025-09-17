8052503f05e60bed2d8d3b19a61b89d0236fb1da
Gijs de Vries <github@janoz.com>
2025-09-18 00:58:29 +0200

    Cleaned up Demo alternative

--- src/main/kotlin/services/announcement/DemoChain.kt
+++ src/main/kotlin/services/announcement/DemoChain.kt
@@ -9,38 +9,35 @@ import kotlin.time.Instant
 
 class DemoChain: StatToAnnouncment  {
 
-    private val PIVOT_DURATION = 11.seconds
+    private val pivotDuration = 11.seconds
 
     private val data:MutableMap<String, Pair<Int, Instant>> = HashMap()
 
     override fun interpret(statMessage: StatMessage,currentTimeStamp: Instant): Announcement {
-
         if (!Events.DEMOLISH.eq(statMessage.event)) return Announcement.NOTHING
-        if (statMessage.player.team?.homeTeam == false) {
-            return if (data.remove(statMessage.matchGUID) != null) {
-                Announcement.COMBO_BREAKER
-            } else {
-                Announcement.NOTHING
-            }
-        }
 
         val streak = data.computeIfAbsent(statMessage.matchGUID) { Pair(0, currentTimeStamp) }
 
-        if (currentTimeStamp.minus(streak.second) < PIVOT_DURATION) {
+        if (statMessage.player.team?.homeTeam == false) {
+            data.remove(statMessage.matchGUID)
+            return if (streak.first > 1) Announcement.COMBO_BREAKER
+            else Announcement.NOTHING
+        }
+
+        return if (currentTimeStamp.minus(streak.second) < pivotDuration) {
             val streakLength = streak.first + 1
             data[statMessage.matchGUID] = Pair(streakLength, currentTimeStamp)
             if (streakLength > 5) return Announcement.MASSACRE
-            return listOf<Announcement>(
-                Announcement.NOTHING,
+            listOf(
                 Announcement.NOTHING,
                 Announcement.DOUBLE_KILL,
                 Announcement.TRIPLE_KILL,
                 Announcement.QUAD_KILL,
                 Announcement.PENTA_KILL
-            )[streakLength]
+            )[streakLength-1]
         } else {
             data.remove(statMessage.matchGUID)
-            return Announcement.NOTHING
+            Announcement.NOTHING
         }
     }
 }
