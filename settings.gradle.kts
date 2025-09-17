c5a586d2fd6d80fb99b5dd85ae021d9e79246182
Gijs de Vries <github@janoz.com>
2025-09-17 16:26:02 +0200

    Trying to get dependency from github

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,11 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         maven("https://m2.dv8tion.net/releases")
-        mavenLocal()
+        maven("https://maven.pkg.github.com/Janoz-NL/discord-voice") {
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
