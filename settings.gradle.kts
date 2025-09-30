bd221c99e14f50de0ef05ca40c31a3b5a2991f27
Gijs de Vries <github@janoz.com>
2025-09-30 13:50:22 +0200

    Update settings.gradle.kts
    Co-authored-by: Robert van Alphen <robertalpha@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "rlannouncer"
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        mavenLocal()
         maven("https://m2.dv8tion.net/releases")
         maven("https://maven.pkg.github.com/Janoz-NL/discord-voice") {
             credentials {
