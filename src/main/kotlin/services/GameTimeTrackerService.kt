2e2c9cbc164e3f50dc2c2e15a71d10118f570bc9
Robert <gitea-git-info@vanalphenict.nl>
2025-10-19 16:12:43 +0200

    minor race condition fix

--- src/main/kotlin/services/GameTimeTrackerService.kt
+++ src/main/kotlin/services/GameTimeTrackerService.kt
@@ -1,16 +1,21 @@
 package nl.vanalphenict.services
 
-import nl.vanalphenict.model.GameTimeMessage
 import kotlin.time.Duration.Companion.seconds
+import nl.vanalphenict.model.GameTimeMessage
 
 class GameTimeTrackerService {
 
     private val gameTimes = HashMap<String, GameTimeMessage>()
 
     fun storeGameTime(message: GameTimeMessage) {
-        gameTimes[message.matchGUID] = message
+        synchronized(this) {
+            gameTimes[message.matchGUID] = message
+        }
     }
 
-    fun getGameTime(matchGUID: String): GameTimeMessage =
-        gameTimes[matchGUID] ?: GameTimeMessage(matchGUID,300.seconds,false)
+    fun getGameTime(matchGUID: String): GameTimeMessage {
+        synchronized(this) {
+            return gameTimes[matchGUID] ?: GameTimeMessage(matchGUID, 300.seconds, false)
+        }
+    }
 }
