c39f5e4df386d7834d59ef493673dbc111851923
Gijs de Vries <github@janoz.com>
2025-10-23 00:52:42 +0200

    Werkend, alleen selecteren theme moet werkend gekregen worden

--- src/main/kotlin/web/page/Root.kt
+++ src/main/kotlin/web/page/Root.kt
@@ -1,23 +1,19 @@
 package nl.vanalphenict.web.page
 
-import io.ktor.server.html.Placeholder
-import io.ktor.server.html.PlaceholderList
 import io.ktor.server.html.Template
-import io.ktor.server.html.TemplatePlaceholder
-import io.ktor.server.html.each
-import io.ktor.server.html.insert
-import kotlinx.html.FlowContent
 import kotlinx.html.HTML
-import kotlinx.html.UL
-import kotlinx.html.article
-import kotlinx.html.b
 import kotlinx.html.body
+import kotlinx.html.classes
 import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.head
+import kotlinx.html.id
+import kotlinx.html.option
 import kotlinx.html.p
 import kotlinx.html.script
+import kotlinx.html.select
 import kotlinx.html.styleLink
+import kotlinx.html.ul
 import nl.vanalphenict.services.ThemeService
 
 class Root {
@@ -26,28 +22,61 @@ class Root {
 
         override fun HTML.apply() {
             head {
-                styleLink(href="web/style/style.css", rel="stylesheet", type="text/css")
-                styleLink(href="https://fonts.googleapis.com", type="text/css", rel="preconnect")
-                styleLink(href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap", rel="stylesheet")
+                styleLink(href = "web/style/style.css", rel = "stylesheet", type = "text/css")
+                styleLink(href = "https://fonts.googleapis.com", type = "text/css", rel = "preconnect")
+                styleLink(
+                    href = "https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap",
+                    rel = "stylesheet"
+                )
 
             }
             body {
+                h1 {
+                    +"Rocket League Announcer"
+                }
+
                 div {
-                    h1 {
-                        + "Rocket League Announcer"
+                    classes = setOf("select")
+                        +"Announcer theme: "
+                    select {
+                        attributes["hx-post"] = "/themes"
+                        attributes["hx-vals"] = "{\"id\": \"#announcerSelect.value\"}"
+                        attributes["hx-swap"] = "outerHTML"
+                        attributes["hx-trigger"] = "change"
+                        id = "announcerSelect"
+                        themeService.themes.forEach { theme ->
+                            option {
+                                value = theme.id.toString()
+                                selected = themeService.selectedTheme == theme
+                                +theme.title
+                            }
+                        }
                     }
+                }
 
-                    p {
-                        + "Theme:"
-                    }
-                    div {
-                        attributes["hx-ext"] = "sse"
-                        attributes["sse-connect"] = "/sse"
-                        attributes["sse-swap"] = "switch_theme"
-                        div { renderThemes(themeService.themes, themeService.selectedTheme) }
-                    }
 
-                    div { renderActions() }
+
+                p {
+                    +"Theme:"
+                }
+
+
+
+                div {
+                    attributes["hx-ext"] = "sse"
+                    attributes["sse-connect"] = "/sse"
+                    attributes["sse-swap"] = "switch_theme"
+                    div { renderThemes(themeService.themes, themeService.selectedTheme) }
+                }
+
+                ul {
+                    id = "actionlist"
+
+                    attributes["hx-ext"] = "sse"
+                    attributes["sse-connect"] = "/sse"
+                    attributes["sse-swap"] = "new_action"
+                    attributes["hx-swap"] = "afterbegin"
+                    attributes["hx-target"] = "#actionlist"
                 }
 
                 val htmx = { e: String -> "assets/htmx.org/dist/$e" }
