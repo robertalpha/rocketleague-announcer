c3e785f5dc60b797f9e5d9fbab445a7330f4b05f
Gijs de Vries <github@janoz.com>
2026-05-12 00:25:44 +0200

    Expanded start announcements (#44)
    * Expanded start announcements
    
    * curly brackets
    
    Co-authored-by: Robert van Alphen <robertalpha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Robert van Alphen <robertalpha@users.noreply.github.com>

--- src/test/kotlin/services/announcement/MatchStartTest.kt
+++ src/test/kotlin/services/announcement/MatchStartTest.kt
@@ -1,8 +1,8 @@
 package nl.vanalphenict.services.announcement
 
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.collections.shouldContainExactly
-import io.kotest.matchers.collections.shouldHaveSize
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.seconds
@@ -24,24 +24,35 @@ class MatchStartTest {
 
     @Test
     fun testListener() {
-        cut.listenTo() shouldContainExactly setOf(GameEvents.ROUND_STARTED)
+        cut.listenTo() shouldContainExactly
+            setOf(GameEvents.ROUND_STARTED, GameEvents.COUNTDOWN_BEGIN)
     }
 
     @Test
     fun interpret() {
 
-        cut.handleGameEvent("2020-08-30T18:43:06Z", startRoundMessage("123"), repo) shouldContain
-            Announcement.MATCH_START
-        cut.handleGameEvent("2020-08-30T18:44:26Z", startRoundMessage("123"), repo) shouldHaveSize 0
-        cut.handleGameEvent("2020-08-30T18:46:26Z", startRoundMessage("123"), repo) shouldHaveSize 0
-
-        cut.handleGameEvent("2020-08-30T19:00:00Z", startRoundMessage("222"), repo) shouldContain
-            Announcement.MATCH_START
-        cut.handleGameEvent("2020-08-30T19:02:00Z", startRoundMessage("222"), repo) shouldHaveSize 0
+        cut.handleGameEvent(
+            "2020-08-30T18:40:06Z",
+            startCountdownMessage("123"),
+            repo,
+        ) shouldContain Announcement.COUNTDOWN_START
+        cut.handleGameEvent("2020-08-30T18:43:06Z", startRoundMessage("123"), repo)
+            .shouldContainAll(Announcement.MATCH_START, Announcement.ROUND_START)
+        cut.handleGameEvent("2020-08-30T18:44:26Z", startRoundMessage("123"), repo) shouldContain
+            Announcement.ROUND_START
+        cut.handleGameEvent("2020-08-30T18:46:26Z", startRoundMessage("123"), repo) shouldContain
+            Announcement.ROUND_START
+        cut.handleGameEvent("2020-08-30T19:00:00Z", startRoundMessage("222"), repo)
+            .shouldContainAll(Announcement.MATCH_START, Announcement.ROUND_START)
+        cut.handleGameEvent("2020-08-30T19:02:00Z", startRoundMessage("222"), repo) shouldContain
+            Announcement.ROUND_START
     }
 
     fun startRoundMessage(matchGuid: String) =
         GameEventMessage(matchGuid = matchGuid, gameEvent = GameEvents.ROUND_STARTED)
+
+    fun startCountdownMessage(matchGuid: String) =
+        GameEventMessage(matchGuid = matchGuid, gameEvent = GameEvents.COUNTDOWN_BEGIN)
 
     private fun MatchStart.handleGameEvent(
         ts: String,
